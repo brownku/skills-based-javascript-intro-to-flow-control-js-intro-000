c345eb8055589838af3bbcbc6227bc4dddce7ea6
Kurt Brown <k.brown1990@yahoo.com>
2020-05-05 14:39:47 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,11 +1,15 @@
 function basicTeenager(age) {
   if(age >= 13 || age <= 19){
-  console.log("You are a teenager!");
+  console.log("You are a teenager!")
 }
 }
 basicTeenager(15);
 function teenager(age) {
-
+  if(age >= 13 || age <= 19){
+  console.log("You are a teenager!")
+}else{
+  console.log("You are not a teenager!")
+}
 }
 
 function ageChecker(age) {
